dfa0f718fd31407d2d6d99923e15dcec765b3d05
Aditya Warekar <adityawarekar1984@gmail.com>
2026-03-05 12:41:21 +0530

    Register and login done

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+console.log("Starting server...");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Forex MERN Backend Running 🚀");
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Server failed:", error);
+  }
+};
+
+startServer();
