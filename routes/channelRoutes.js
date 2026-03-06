5b2def1014d6b332b94d93f05f6c8083e155a900
Aditya Warekar <adityawarekar1984@gmail.com>
2026-03-06 22:35:20 +0530

    remove duplicate backend folders from root

--- routes/channelRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createChannel } = require("../controllers/channelController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/create", authMiddleware, createChannel);
-
-module.exports = router;
--- routes/forexRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendForexUpdate } = require("../controllers/forexController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.post("/update", authMiddleware, sendForexUpdate);
-
-module.exports = router;
